ca67bf133066ce544ef21a349cb96c3b11482738
Maxime Larrivée-Roy <zmotrin@gmail.com>
2024-05-16 15:13:44 -0400

    New ini settings

--- embeder.cpp
+++ embeder.cpp
@@ -76,6 +76,15 @@ static void embeded_ini_defaults(HashTable *configuration_hash)
 
 	ZVAL_NEW_STR(&ini_value, zend_string_init(ZEND_STRL("UTF-8"), 1));
 	zend_hash_str_update(configuration_hash, ZEND_STRL("iconv.internal_encoding"), &ini_value);
+
+	ZVAL_NEW_STR(&ini_value, zend_string_init(ZEND_STRL("0"), 1));
+	zend_hash_str_update(configuration_hash, ZEND_STRL("phar.readonly"), &ini_value);
+
+	ZVAL_NEW_STR(&ini_value, zend_string_init(ZEND_STRL("0"), 1));
+	zend_hash_str_update(configuration_hash, ZEND_STRL("zlib.output_compression"), &ini_value);
+
+	ZVAL_NEW_STR(&ini_value, zend_string_init(ZEND_STRL("3072M"), 1));
+	zend_hash_str_update(configuration_hash, ZEND_STRL("memory_limit"), &ini_value);
 }
 
 
